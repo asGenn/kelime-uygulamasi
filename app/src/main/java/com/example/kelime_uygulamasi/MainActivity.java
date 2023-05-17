821a49098d7d7a9d10a4320f8de89fe806367419
asGenn <abdulsametgencc@gmail.com>
2023-05-17 14:37:41 +0300

    Project Started

---
+++ app/src/main/java/com/example/kelime_uygulamasi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kelime_uygulamasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
