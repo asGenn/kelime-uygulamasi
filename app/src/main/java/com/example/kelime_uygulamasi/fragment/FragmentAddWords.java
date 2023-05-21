67d480f1b61e4c41d44bb5a1ee06d2c0e7cbe9d7
UtkuYURT <213405075@ogrenci.ibu.edu.tr>
2023-05-21 16:35:55 +0300

    ui bugları çözüldü

--- app/src/main/java/com/example/kelime_uygulamasi/fragment/FragmentAddWords.java
+++ app/src/main/java/com/example/kelime_uygulamasi/fragment/FragmentAddWords.java
@@ -1,6 +1,9 @@
 package com.example.kelime_uygulamasi.fragment;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -8,13 +11,15 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 import android.widget.Toast;
+
 import com.example.kelime_uygulamasi.R;
-import com.example.kelime_uygulamasi.adaptor.myRecyAdaptor;
 import com.example.kelime_uygulamasi.databinding.FragmentAddWordsBinding;
 import com.example.kelime_uygulamasi.models.Deneme;
 import com.example.kelime_uygulamasi.repository.MyRecyAdaptor;
@@ -22,12 +27,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -38,6 +42,7 @@ public class FragmentAddWords extends Fragment {
 
     private FragmentAddWordsBinding binding;
     private RecyclerView myRecy;
+    private static int userWordCount = 0;
     private com.example.kelime_uygulamasi.repository.MyRecyAdaptor myRecyAdaptor;
     private FirebaseFirestore mFirestore=FirebaseFirestore.getInstance();
     private ArrayList<Deneme> kuluplers;
@@ -45,7 +50,7 @@ public class FragmentAddWords extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAddWordsBinding.inflate(getLayoutInflater(), container, false);
-        myRecy = (RecyclerView) binding.myRecy;
+        myRecy = binding.myRecy;
         kuluplers=new ArrayList<>();
         myRecyAdaptor=new MyRecyAdaptor(kuluplers);
         myRecy.setAdapter(myRecyAdaptor);
--- app/src/main/java/com/example/kelime_uygulamasi/fragment/FragmentHomePage.java
+++ app/src/main/java/com/example/kelime_uygulamasi/fragment/FragmentHomePage.java
@@ -3,8 +3,6 @@ package com.example.kelime_uygulamasi.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
