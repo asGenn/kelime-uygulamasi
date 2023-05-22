a9616e99ca28a0850a2f4dd5e068bd1654e71e99
UtkuYURT <213405075@ogrenci.ibu.edu.tr>
2023-05-22 23:51:41 +0300

    tek kelime hikaye

--- app/src/main/java/com/example/kelime_uygulamasi/fragment/FragmentHomePage.java
+++ app/src/main/java/com/example/kelime_uygulamasi/fragment/FragmentHomePage.java
@@ -1,17 +1,30 @@
 package com.example.kelime_uygulamasi.fragment;
 
+import static androidx.fragment.app.FragmentManager.TAG;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kelime_uygulamasi.databinding.FragmentHomePageBinding;
 import com.example.kelime_uygulamasi.repository.ChatGptRepository;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+import java.util.Map;
 
 public class FragmentHomePage extends Fragment {
 
@@ -22,15 +35,16 @@ public class FragmentHomePage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         tasarim = FragmentHomePageBinding.inflate(getLayoutInflater(), container, false);
         chatGptRepository = new ChatGptRepository();
-        tasarim.button.setOnClickListener(new View.OnClickListener() {
+        wordList();
+        tasarim.buttonHikaye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 chatGptRepository.generateWordDetail();
                 chatGptRepository.generateWordDetail().observe(getViewLifecycleOwner(), new Observer<String>() {
                     @Override
                     public void onChanged(String s) {
-                        tasarim.textView.setText(s);
-                        tasarim.button.setVisibility(View.GONE);
+                        tasarim.textViewHikaye.setText(s);
+                        tasarim.buttonHikaye.setVisibility(View.GONE);
                     }
                 });
 
@@ -44,5 +58,43 @@ public class FragmentHomePage extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    public void wordList(){
+        FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String userId = mAuth.getUid();
+
+        mFirestore.collection("User").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    List<Map<String, Object>> wordList = (List<Map<String, Object>>) documentSnapshot.get("wordList");
+
+                    if (wordList != null) {
+                        StringBuilder kelimeBuilder = new StringBuilder();
+                        StringBuilder anlamBuilder = new StringBuilder();
+
+                        for (Map<String, Object> word : wordList) {
+                            String kelime = (String) word.get("word");
+                            String anlam = (String) word.get("mean");
+
+                            kelimeBuilder.append("- ").append(kelime).append("\n");
+                            anlamBuilder.append(anlam).append("\n");
+                        }
+
+                        String kelimelerText = kelimeBuilder.toString().trim();
+                        String anlamlarText = anlamBuilder.toString().trim();
+
+                        tasarim.textViewKelime.setText(kelimelerText);
+                        tasarim.textViewAnlamlar.setText(anlamlarText);
+                    } else {
+                        Log.d(TAG, "Kullanıcının eklediği kelime yok.");
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kelime_uygulamasi/repository/ChatGptRepository.java
+++ app/src/main/java/com/example/kelime_uygulamasi/repository/ChatGptRepository.java
@@ -1,15 +1,32 @@
 package com.example.kelime_uygulamasi.repository;
 
 
+import static androidx.fragment.app.FragmentManager.TAG;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Single;
@@ -20,7 +37,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ChatGptRepository {
     private final String model = "gpt-3.5-turbo";
-    private final String  API_KEY = "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";
+    private final String API_KEY = "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";
     private final OpenAiService service = new OpenAiService(API_KEY);
     private ChatCompletionRequest request;
     private ChatCompletionResult result;
@@ -32,43 +49,87 @@ public class ChatGptRepository {
         story = new MutableLiveData<>();
     }
 
-    public MutableLiveData<String> generateWordDetail(){
-        ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "you are english random word genator" );
-        messages.add(systemMessage);
-        ChatMessage userMessage = new ChatMessage(ChatMessageRole.USER.value(), "give me random word");
-        messages.add(userMessage);
-        Single.fromCallable(() -> {
-                    request = ChatCompletionRequest
-                            .builder()
-                            .messages(messages)
-                            .model(model)
-                            .temperature(1.0)
-                            .topP(1.0)
-                            .build();
-                     result = service.createChatCompletion(request);
-                    return result;
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<ChatCompletionResult>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+    public MutableLiveData<String> generateWordDetail() {
+        FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String userId = mAuth.getUid();
 
+        MutableLiveData<String> story = new MutableLiveData<>();
+
+        mFirestore.collection("User").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onComplete(@androidx.annotation.NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    List<Map<String, Object>> wordList = (List<Map<String, Object>>) documentSnapshot.get("wordList");
+
+                    if (wordList != null && !wordList.isEmpty()) {
+                        JSONArray jsonWords = new JSONArray();
+                        for (Map<String, Object> word : wordList) {
+                            jsonWords.put(word.toString());
+                        }
+
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+                            jsonObject.put("words", jsonWords);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        String jsonString = jsonObject.toString();
+
+                        ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.name().toLowerCase(), "your are english story teller. \n" +
+                                "I'll give you Json format :\n" +
+                                "{\n" +
+                                "       \"words\":[\n" +
+                                "           ]\n" +
+                                "}\n" +
+                                "you have to return Json format like this :\n" +
+                                "{\n" +
+                                "    \"story\":\"\"\n" +
+                                "\n" +
+                                "}");
+                        messages.add(systemMessage);
+                        ChatMessage userMessage = new ChatMessage(ChatMessageRole.USER.name().toLowerCase(), jsonString);
+                        messages.add(userMessage);
+
+                        Single.fromCallable(() -> {
+                                    request = ChatCompletionRequest
+                                            .builder()
+                                            .messages(messages)
+                                            .model(model)
+                                            .temperature(1.0)
+                                            .topP(1.0)
+                                            .build();
+                                    result = service.createChatCompletion(request);
+                                    return result;
+                                })
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new SingleObserver<ChatCompletionResult>() {
+                                    @Override
+                                    public void onSubscribe(@NonNull Disposable d) {
+
+                                    }
+
+                                    @Override
+                                    public void onSuccess(@NonNull ChatCompletionResult chatCompletionResult) {
+                                        story.postValue(chatCompletionResult.getChoices().get(0).getMessage().getContent());
+                                    }
+
+                                    @Override
+                                    public void onError(@NonNull Throwable e) {
+                                        e.printStackTrace();
+                                    }
+                                });
+                    } else {
+                        Log.d(TAG, "Kullanıcının eklediği kelime yok.");
                     }
-                    @Override
-                    public void onSuccess(@NonNull ChatCompletionResult chatCompletionResult) {
+                }
+            }
+        });
 
-
-                        story.postValue(chatCompletionResult.getChoices().get(0).getMessage().getContent());
-
-                    }
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        e.printStackTrace();
-                    }
-                });
         return story;
-
     }
 
 }
